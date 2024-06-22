1e42f806f78583f98f4aa08e6c7b01e67158d2af
Vladimir Andrusevich <vl.andrusevich@yandex.by>
2024-06-22 22:51:45 +0300

    Initial commit

---
+++ src/main/java/com/andrusevich/smartshop/admin/SmartShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.andrusevich.smartshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartShopBackApplication.class, args);
+    }
+
+}
